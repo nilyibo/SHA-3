ada0bb3151bf2051f506b654d82843b71b43662b
Zhengqi Yang <zyang36@illinois.edu>
2015-05-06 15:56:37 -0500

    added keccak.cpp

---
+++ keccak.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <numeric>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <cstring>
+#include <string>
+#include <map>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <bitset>
+#include <queue>
+#include <sstream>
+#include <deque>
+#include <cassert>
+
+
+using namespace std; 
+
+
+class Keccak {
+	private: 
+	
+
+	public: 
+	//Constructions 
+	public Keccak() {
+	}
+
+}
