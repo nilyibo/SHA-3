bc850f0e61e9b5211de9b8bf1020f6a692a5a544
Yibo Guo <nil.yibo@gmail.com>
2015-05-06 18:30:58 -0500

    Added a simple example.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "keccak.cpp"
+
+using namespace std;
+
+int main()
+{
+    Keccak k;
+    k.setB(1600);
+    string input = "This is a test string";
+    string output = k.hash(input);
+    cout << "Input: " << input << endl
+        << "Output: " << output << endl;
+    return 0;
+}
