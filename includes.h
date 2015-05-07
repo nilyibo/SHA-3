5a977cdbd2719b59ca1a0642b3964453b0abf2b5
Zhengqi Yang <zyang36@illinois.edu>
2015-05-06 20:35:54 -0500

    Added includes.h

---
+++ includes.h
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <numeric>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <cstring>
+#include <string>
+#include <map>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <bitset>
+#include <queue>
+#include <sstream>
+#include <deque>
+#include <cassert>
+
+using namespace std;
